ac330d30a0e84cd59f6ccb46281a185e30e755e2
kelvinzou <xuanzou1991@gmail.com>
2014-10-26 19:40:15 -0400

    update the socket with select

--- emulator_code/clientUDP.c
+++ emulator_code/clientUDP.c
@@ -12,40 +12,77 @@
 #include <sys/types.h>
 #include <time.h> 
 
+#define INTI_TO 10000
 int main(int argc, char**argv)
 {
-   int sockfd,n;
-   struct sockaddr_in servaddr,cliaddr;
-  // char sendline[1000];
-   char recvline[1500];
+	int sockfd,n;
+	struct sockaddr_in servaddr,cliaddr;
 
-   if (argc < 2)
-   {
-      printf("usage:  udpcli <IP address>\n");
-      exit(1);
-   }
-
-   sockfd=socket(AF_INET,SOCK_DGRAM,0);
-
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=inet_addr(argv[1]);
-   servaddr.sin_port=htons(1234);
-
-   while (1)
-   {
-      sleep(2);
-      char  sendline[1400] ;
-	memset(sendline, 0x41,100);
-	
-      int round =10000;
-	for (round=1; round>0; round--)
-	{  sendto(sockfd,sendline,100,0,(struct sockaddr *)&servaddr,sizeof(servaddr));}
-      
-     if(argc==3){ 
-      n=recvfrom(sockfd,recvline,1500,0,NULL,NULL);
-      recvline[n]=0;
-      fputs(recvline+28,stdout);
+	if (argc < 2)
+	{
+		printf("usage:  udpcli <IP address>\n");
+		exit(1);
 	}
-   }
+
+	sockfd=socket(AF_INET,SOCK_DGRAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+	servaddr.sin_port=htons(63000);
+
+// Here we use file descriptor to avoid I/O block at receive side.
+	int timeout =10000;
+	struct timeval tv;
+	fd_set readfds, active_fs;
+	tv.tv_sec = 0;
+	tv.tv_usec = 10000;
+	FD_ZERO(&readfds);
+	FD_SET(sockfd, &readfds);
+
+
+
+	struct timeval t1, t2;
+	double elapsedTime;
+	gettimeofday(&t1, NULL);
+
+
+	while (1)
+	{
+		
+		char sendline[1400];
+		char recvline[1400];
+		memset(sendline, 0x41,1399);
+
+		printf("Source address for receive from is %s",inet_ntoa(*(struct in_addr*) &cliaddr.sin_addr.s_addr));
+		printf(" and %s\n",inet_ntoa(*(struct in_addr*) &servaddr.sin_addr.s_addr));
+
+		
+
+		sendto(sockfd,sendline,1400,0,(struct sockaddr *)&servaddr,sizeof(servaddr));
+		
+		
+		active_fs = readfds;
+		select(sockfd+1, &active_fs, NULL, NULL, &tv);
+		if(!FD_ISSET(sockfd, &active_fs)){
+			printf("Not ready for read yet, skip\n");
+			printf("Timeout is now %d\n", timeout);
+			usleep(timeout);
+			timeout =  timeout *2;
+		} else {
+			n=recvfrom(sockfd,recvline,1399,0,NULL,NULL);
+			recvline[n]=0;
+			fputs(recvline,stdout);
+			timeout = INTI_TO;
+			gettimeofday(&t1, NULL);
+		}
+		
+
+		gettimeofday(&t2, NULL);
+		elapsedTime =(t2.tv_sec - t1.tv_sec);
+		if (elapsedTime>30){
+			break;
+		}
+
+	}
 }
--- emulator_code/serverUDP.c
+++ emulator_code/serverUDP.c
@@ -9,37 +9,106 @@
 #include <sys/types.h>
 #include <time.h> 
 
-
+#include <pthread.h>
+#include <sys/time.h>
 
 /* Sample UDP server */
 
+ typedef struct
+{
+    int id;
+    char* str;
+} threadParameter;
+/*
+void * handleConnection(void * ptr) {
+
+	char * parameter = (char *) ptr;
+	printf("%s\n", parameter);
+}
+*/
+
+
+void handleRequest(char * request, char * response, int n){
+	printf("data is %s\0", request);
+	memcpy(response, request, (size_t) n);
+	*(response +n) = 0;
+}
+
+
 int main(int argc, char**argv)
 {
-   int sockfd,n;
-   struct sockaddr_in servaddr,cliaddr;
-   socklen_t len;
-   char mesg[1000];
+	/*
+//initialize the threads	
+	int counter  = 0;
+   	pthread_t threads[10000];
+   	const char * msg = "child thread";
+  	//here we keep a queue of idle threads in queue idle, 
+  	//and everytime we grab a thread id from the idle pool and create a thread to run it
 
-   sockfd=socket(AF_INET,SOCK_DGRAM,0);
+   	queue<int> idle;
+   	int i =0;
+   	for(i=0; i<1000; i++){
+   		idle.push(i);
+   	}
+*/
 
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-   servaddr.sin_port=htons(1234);
-   bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+//keep track of time, and exits with a certain interval, since we cannot kill it in mininet
+   	struct timeval t1, t2;
+    double elapsedTime;
+    gettimeofday(&t1, NULL);
+//initialize the socket
+    struct timeval tv;
+    fd_set readfds, active_fs;
+    tv.tv_sec = 0;
+    tv.tv_usec = 100000;
+    FD_ZERO(&readfds);
 
-   while(1)
-   {
-      len = sizeof(cliaddr);
-      n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-      printf("Source address for receive from is %s and %s\n", 
-         inet_ntoa( *(struct in_addr* ) &cliaddr.sin_addr.s_addr), 
-         inet_ntoa( *(struct in_addr* ) &servaddr.sin_addr.s_addr));
-      sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-      printf("-------------------------------------------------------\n");
-      mesg[n] = 0;
-      printf("Received the following:\n");
-      printf("%s",(char*)(mesg));
-      printf("-------------------------------------------------------\n");
-   }
+
+   	int port_count = 1025;
+   	int sockfd,n;
+   	struct sockaddr_in servaddr,cliaddr;
+   	socklen_t len;
+   
+	sockfd=socket(AF_INET,SOCK_DGRAM,0);
+	FD_SET(sockfd, &readfds);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(63000);
+	bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+	
+	//select(sockfd+1, &readfds, NULL, NULL, &tv);
+	int drop =0;
+	while(1){
+		len = sizeof(cliaddr);
+		char mesg[1400];
+		char response[1400];
+		active_fs = readfds;
+		select(sockfd+1, &active_fs, NULL, NULL, &tv);
+		
+		if(FD_ISSET(sockfd, &active_fs)){
+			n = recvfrom(sockfd,mesg,1399,0,(struct sockaddr *)&cliaddr,&len);
+			printf("Source address for receive from is %s",inet_ntoa(*(struct in_addr*) &cliaddr.sin_addr.s_addr));
+			printf(" and %s\n",inet_ntoa(*(struct in_addr*) &servaddr.sin_addr.s_addr));
+			mesg[n]=0;;
+			handleRequest(mesg, response, n);
+			if (drop <8) {
+				drop++;
+			}
+			else {
+				sendto(sockfd,response,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+				drop =0;
+			}
+		}
+		
+		//usleep(10000);
+		gettimeofday(&t2, NULL);
+   	 	elapsedTime =(t2.tv_sec - t1.tv_sec);
+   	 	if (elapsedTime>600){
+   	 		break;
+   	 	}
+	}
+
 }
+
