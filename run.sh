e31bc8b721e3245ad96736bc8d3828d11e07d889
kelvinzou <xuanzou1991@gmail.com>
2015-05-13 20:22:01 -0400

    add running client

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output01.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output02.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output03.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output04.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output05.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output06.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output07.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output08.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output09.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output10.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output11.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output12.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output13.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output14.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output15.txt &
+iperf -c 10.0.3.2 -p 5002 -P 64 -t 20 >output16.txt &
+
